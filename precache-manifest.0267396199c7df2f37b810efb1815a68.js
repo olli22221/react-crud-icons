8aa2d6a752efd94426ddb64546f4d7cf9fad26d7
evoluteur <ogiulieri@gmail.com>
2020-07-22 16:29:02 -0700

    First build.

---
+++ precache-manifest.0267396199c7df2f37b810efb1815a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b94f28a5f4a18a7b9ef2a030835b5",
+    "url": "/evoluteur/react-crud-icons-demo/index.html"
+  },
+  {
+    "revision": "3ea03a4ad99a32cfea55",
+    "url": "/evoluteur/react-crud-icons-demo/static/css/2.ed230628.chunk.css"
+  },
+  {
+    "revision": "2f11e8b395c88cfb88bc",
+    "url": "/evoluteur/react-crud-icons-demo/static/css/main.14d21322.chunk.css"
+  },
+  {
+    "revision": "3ea03a4ad99a32cfea55",
+    "url": "/evoluteur/react-crud-icons-demo/static/js/2.1fc8249f.chunk.js"
+  },
+  {
+    "revision": "2f11e8b395c88cfb88bc",
+    "url": "/evoluteur/react-crud-icons-demo/static/js/main.68fe4392.chunk.js"
+  },
+  {
+    "revision": "8ca803f6a3787648610c",
+    "url": "/evoluteur/react-crud-icons-demo/static/js/runtime~main.6deb02bc.js"
+  }
+]);
